e9f450c4a8d1551557e81f194fd1645b0b6c7ba3
alin-rautoiu <alin.rautoiu@cti.pub.ro>
2013-11-20 10:34:38 +0200

    Refactorizare
    Si ceva identare + culori schimbate

--- script.js
+++ script.js
@@ -1,48 +1,38 @@
-
-		$(function(){
-			$('#adaugare').click(function(){
-				$('.formular').show();
-			});
-			$('#submit').click(function(e){
-				e.preventDefault();
-				$('.mainTable tr:last').after(
-				'<tr><td id="name">'+ $('#name_form').val() + '</td><td id="prenume">'+ $('#surname_form').val() +'</td><td id="varsta">'+$('#age_form').val() +'</td><td id="departament">'+$('#dep_form').val() + '</td><td id="angajare">'+$('#date_form').val() + '</td><td id="post">'+$('#post_form').val()+'</td><td>'
-					);
-				$('#name_form').val('');
-				$('#surname_form').val('');
-				$('#age_form').val('');
-				$('#dep_form').val('');
-				$('#date_form').val('');
-				$('#post_form').val('');
-				$('.formular').fadeOut();
-
-				$('.mainTable td').click(function(){
-					$(this).html('');
-				});
-				$('.mainTable td').click(function(){
-				$(this).html('');
-				var non_empty_cells_count = 0;
-				$(this).siblings().each(function(){
-					if($(this).html() !== ''){
-						non_empty_cells_count++;	
-					}
-				});
-				if(non_empty_cells_count === 0) {
-					$(this).parent().hide();
-				}
-				});
-			});
-
-			$('.mainTable td').click(function(){
-				$(this).html('');
-				var non_empty_cells_count = 0;
-				$(this).siblings().each(function(){
-					if($(this).html() !== ''){
-						non_empty_cells_count++;	
-					}
-				});
-				if(non_empty_cells_count === 0) {
-					$(this).parent().hide();
-				}
-			});
+var hideOnClick = function(){
+	$(this).html('');
+	var non_empty_cells_count = 0;
+	
+	$(this).siblings().each(function(){
+		if($(this).html() !== ''){
+			non_empty_cells_count++;	
+		}
 	});
+
+	if(non_empty_cells_count === 0) {
+		$(this).parent().hide();
+	}
+}
+
+$(function(){
+	$('#adaugare').click(function(){
+		$('.formular').show();
+	});
+
+	$('#submit').click(function(e){
+		e.preventDefault();
+		$('.mainTable tr:last').after(
+		'<tr><td id="name">'+ $('#name_form').val() + '</td><td id="prenume">'+ $('#surname_form').val() +'</td><td id="varsta">'+$('#age_form').val() +'</td><td id="departament">'+$('#dep_form').val() + '</td><td id="angajare">'+$('#date_form').val() + '</td><td id="post">'+$('#post_form').val()+'</td><td>'
+			);
+		$('#name_form').val('');
+		$('#surname_form').val('');
+		$('#age_form').val('');
+		$('#dep_form').val('');
+		$('#date_form').val('');
+		$('#post_form').val('');
+		$('.formular').fadeOut();
+		
+		$('.mainTable tr:last td').click(hideOnClick);
+	});
+
+	$('.mainTable td').click(hideOnClick);
+});
