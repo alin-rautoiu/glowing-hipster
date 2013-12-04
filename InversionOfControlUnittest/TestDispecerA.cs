b6203226de9ab92ea42e7683590e9ae937648ad8
alin-rautoiu <alin.rautoiu@cti.pub.ro>
2013-12-04 18:40:09 +0200

    Mock

---
+++ InversionOfControlUnittest/TestDispecerA.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using InversionOfControlExemple;
+
+namespace InversionOfControlUnittest
+{
+    [TestClass]
+    public class TestDispecerA
+    {
+        [TestMethod]
+        public void GivenDispeceratWhenCallAddPersonThenTheMethodAddFromISursaDeDateIsCalled()
+        {
+            //arrange
+            var sursaDeDateMock = new Mock<ISursaDeDate>();
+            var dispA = new DispecerA(sursaDeDateMock.Object);
+
+            //act
+            dispA.AddPerson();
+
+            //assert
+            sursaDeDateMock.Verify(v => v.add(It.IsAny<Persoana>()), Times.Exactly(1));
+        }
+    }
+}
